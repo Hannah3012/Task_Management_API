87cffbc7f1d2606a72c9687529ef989fa6ebba8d
Hanna Tariku <hannatariku1996@gmail.com>
2025-10-27 20:02:31 +0300

    Created TaskViewSet to handle CRUD with IsOwnerOrReadOnly custom permission.

--- task_management_api/tasks/views.py
+++ task_management_api/tasks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # read-only allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(owner=user)
+
