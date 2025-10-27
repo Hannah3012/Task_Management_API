7eeedfb8233d9a14f9fc4df21f157ac27c7e611a
Hanna Tariku <hannatariku1996@gmail.com>
2025-10-27 20:19:32 +0300

    feat(tasks): add serializers for Task and Comment models

--- task_management_api/tasks/serializers.py
+++ task_management_api/tasks/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+
+    class Meta:
+        model = Comment
+        fields = ["id", "task", "author", "content", "created_at", "updated_at"]
+        read_only_fields = ["id", "author", "created_at", "updated_at"]
 
 class TaskSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="owner.username")
+    author = serializers.ReadOnlyField(source="author.username")
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Task
-        fields = ["id", "title", "description", "owner", "status", "due_date", "created_at", "updated_at"]
-        read_only_fields = ["id", "owner", "created_at", "updated_at"]
+        fields = [
+            "id", "title", "description", "status", "due_date", 
+            "author", "comments", "created_at", "updated_at"
+        ]
+        read_only_fields = ["id", "author", "created_at", "updated_at"]
